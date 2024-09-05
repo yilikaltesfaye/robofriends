33fabebb36ddd9a0d4cb4dfceda38e37299def94
Yilikal Tesfaye Kebede <yilikalt.kebede@gmail.com>
2024-09-05 18:04:48 +0300

    Added Card.js {implemented props } and cleaned up the src folder

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import Card from "./Card";
+import "tachyons";
+import { robots } from "./robots";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-    <h1>Hello WOrld</h1>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} username={robots[0].username}/>
+		<Card id={robots[1].id} name={robots[1].name} email={robots[1].email} username={robots[1].username}/>
+		<Card id={robots[2].id} name={robots[2].name} email={robots[2].email} username={robots[2].username}/>
+		<Card id={robots[3].id} name={robots[3].name} email={robots[3].email} username={robots[3].username}/>
+	</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
