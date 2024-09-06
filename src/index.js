fdf524e149e4255ff8de123911b68fa8979a75d8
Yilikal Tesfaye Kebede <yilikalt.kebede@gmail.com>
2024-09-06 09:26:07 +0300

    Added CardList and Mapped through the robots.js objects

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
-import Card from "./Card";
 import "tachyons";
 import { robots } from "./robots";
+import CardList from "./CardList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<Card id={robots[0].id} name={robots[0].name} email={robots[0].email} username={robots[0].username}/>
-		<Card id={robots[1].id} name={robots[1].name} email={robots[1].email} username={robots[1].username}/>
-		<Card id={robots[2].id} name={robots[2].name} email={robots[2].email} username={robots[2].username}/>
-		<Card id={robots[3].id} name={robots[3].name} email={robots[3].email} username={robots[3].username}/>
+		<CardList robots={robots} />
 	</React.StrictMode>
 );
 
